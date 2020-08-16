93122af9df5bd0560fee7026d687165ba34f6fea
yungo1846 <yungo1468@naver.com>
2020-08-16 23:30:07 +0900

    sentry1

--- config/urls.py
+++ config/urls.py
@@ -18,8 +18,10 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 def trigger_error(request):
-    division_by_zero = 1 / 
+    division_by_zero = 1 / 0
+
 
 urlpatterns = [
     path("", include("core.urls", namespace="core")),
